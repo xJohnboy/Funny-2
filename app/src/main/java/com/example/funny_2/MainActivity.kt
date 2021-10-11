e5d9af55d80b1233047218be76b91991a92f9a45
Johnboy <damrongsak17@gmail.com>
2021-10-11 14:33:54 +0700

    Funny-2

--- app/src/main/java/com/example/funny_2/MainActivity.kt
+++ app/src/main/java/com/example/funny_2/MainActivity.kt
@@ -20,29 +20,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val navView: BottomNavigationView = binding.navView
         navView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_store -> {
-                    viewPager.setCurrentItem(0,false)
+                    viewPager.setCurrentItem(0, false)
                     return@setOnItemSelectedListener true
                 }
                 R.id.navigation_map -> {
-                    viewPager.setCurrentItem(1,false)
+                    viewPager.setCurrentItem(1, false)
                     return@setOnItemSelectedListener true
                 }
                 R.id.navigation_account -> {
-                    viewPager.setCurrentItem(2,false)
+                    viewPager.setCurrentItem(2, false)
                     return@setOnItemSelectedListener true
                 }
                 else -> false
             }
         }
+
         viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        initFragment()
         viewPager.adapter = viewPagerAdapter
+        viewPager.isUserInputEnabled = false
+    }
+
+    private fun initFragment() {
         viewPagerAdapter.addFragment(StoreFragment())
         viewPagerAdapter.addFragment(MapFragment())
         viewPagerAdapter.addFragment(AccountFragment())
-        viewPager.isUserInputEnabled = false
     }
 }
